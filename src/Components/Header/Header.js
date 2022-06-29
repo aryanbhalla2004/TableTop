b6214d659da274811c044f04435dd4cbba9f6af7
Nicholas <nicholashacaulttabs@gmail.com>
2022-06-29 16:03:50 -0500

    Add initial login page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>Header</div>
+    <>
+      <h1>Header</h1>
+      {props.CurrentUser && <Link to="" onClick={props.Logout}>Logout</Link>}
+      <h4>{props.CurrentUser && props.CurrentUser.email}</h4>
+    </>
   )
 }
 
--- src/Pages/Auth/Login/Login.js
+++ src/Pages/Auth/Login/Login.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
+  const history = useNavigate();
+
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: ""
+  });
+
+  const updateInput = (e) => {
+    setUserInput(prevInput => ({
+      ...prevInput, [e.target.name]: e.target.value
+    }));
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if (userInput.email === "" || userInput.password === "") {
+      console.log("Fields are empty");
+    } else {
+      console.log("doing a firebase request");
+      try {
+        let userDetails = await props.Login(userInput.email, userInput.password);
+        console.log("found user");
+        if (userDetails.user.emailVerified) {
+          history("/");
+        } else {
+          history("/email-activation");
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={onSubmit}>
+      <input type="email" name="email" placeholder='john@yahoo.com' value={userInput.email} onChange={updateInput} />
+      <input type="password" name="password" placeholder='password' value={userInput.password} onChange={updateInput} />
+      <button>Click me</button>
+    </form>
   )
 }
 
