9705a34ea24d0cfed9fba16454b504645da8dcca
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-07-07 11:17:32 -0500

    add NavbarSearchForm and TopNavbar components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SideBarMenu } from "./components/SideBarMenu/SideBarMenu";
+import { TopNavbar } from "./components/TopNavbar/TopNavbar";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Landing Page</div>} />=
-        <Route path="/dashboard" element={<SideBarMenu />} />
+        <Route path="/" element={<div>Landing Page</div>} />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <SideBarMenu />
+              <TopNavbar />
+            </>
+          }
+        />
       </Routes>
     </Router>
   );
---
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -0,0 +1,27 @@
+import styles from "../TopNavbar/TopNavbar.module.css";
+import { VscBellDot } from "react-icons/vsc";
+import { IoLogOutOutline } from "react-icons/io5";
+import { NavbarSearchForm } from "../NavbarSearchForm/NavbarSearchForm";
+const TopNavbar = () => {
+  return (
+    <div className={styles["top_navbar"]}>
+      <NavbarSearchForm />
+      <div className={styles["right_nav_options"]}>
+        <VscBellDot />
+        <IoLogOutOutline />
+        <div className={styles["line"]}></div>
+        <img
+          src="https://placekitten.com/200/300"
+          alt="profile photo"
+          className={styles["profile_img"]}
+        />
+        <div className={styles["name_and_role"]}>
+          <div className={styles["username"]}>Grandpa Joe</div>
+          <div className={styles["role"]}>Admin</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { TopNavbar };
