a77838ed0b3534327438e0511ff85094eb65dda4
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-07-09 20:40:58 -0500

    add styles and links to sidebar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -9,23 +9,55 @@ import {
   SubMenu,
 } from "react-pro-sidebar";
 
-import { FaGem, FaHeart } from "react-icons/fa";
+import * as FaIcons from "react-icons/fa";
+import * as BiIcons from "react-icons/bi";
+import * as AiIcons from "react-icons/ai";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+  const [collapsedStatus, setCollapsedStatus] = useState(false);
+  const handleClickOnSideBar = (e) => {
+    setCollapsedStatus(!collapsedStatus);
+  };
   return (
-    <ProSidebar className="sidebar">
-      <SidebarHeader>TableTop</SidebarHeader>
-      <SidebarContent>
-        <Menu iconShape="circle">
-          <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-          <SubMenu title="Components" icon={<FaHeart />}>
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-        </Menu>
-      </SidebarContent>
-      <SidebarFooter>Footer</SidebarFooter>
-    </ProSidebar>
+    <>
+      <ProSidebar className="sidebar" collapsed={collapsedStatus}>
+        <SidebarHeader className="sidebar_header">
+          <BiIcons.BiMessage />
+          <div
+            className={
+              collapsedStatus ? "company_name hide_name" : "company_name"
+            }
+          >
+            TableTop
+          </div>
+        </SidebarHeader>
+        <SidebarContent>
+          <Menu>
+            <MenuItem icon={<FaIcons.FaGem />}>
+              <Link to={"home"} />
+              Home
+            </MenuItem>
+            <MenuItem icon={<AiIcons.AiOutlineMessage />}>
+              <Link to={"messages"} />
+              Messages
+            </MenuItem>
+            <SubMenu title="Favorite" icon={<FaIcons.FaHeart />}>
+              <MenuItem>Component 1</MenuItem>
+              <MenuItem>Component 2</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+        <SidebarFooter className="sidebar_footer">Footer</SidebarFooter>
+      </ProSidebar>
+      <AiIcons.AiOutlineRight
+        className={
+          collapsedStatus ? "right_arrow menu_close" : "right_arrow menu_open"
+        }
+        onClick={handleClickOnSideBar}
+      />
+    </>
   );
 };
 export default Sidebar;
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -12,7 +12,17 @@ const Dashboard = () => {
     // 	<p><Link to="/dashboard/business/profile">Profile</Link></p>
     // 	<Outlet />
     // </div>
-    <Sidebar></Sidebar>
+    <div style={{ display: "flex" }}>
+      <Sidebar></Sidebar>
+      <div>
+        <h1>Content</h1>
+        You'll find the CSS styles used in Revindex Storefront follows closely
+        the Bootstrap 3.x and DNN UX standard therefore making it very easy to
+        style module elements simply by overriding the style rules in your
+        portal CSS. Please see the video tutorial below on how to override
+        styles from your portal CSS.
+      </div>
+    </div>
   );
 };
 
